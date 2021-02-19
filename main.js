db1667a1915f3ff23bd0524129a372534719481e
Minahil shafique <minahal.shafique@gmail.com>
2021-02-19 13:50:28 -0500

    15 Functions

--- main.js
+++ main.js
@@ -2,9 +2,53 @@
  * YOUR CODE BELOW! *
  ********************/
 
+function moreThan5(num){ return num > 5}
 
+function isNewTopScore(myScore , topScore) { 
+  return myScore > topScore
+}
+function isInDanger(gradeNumber){
+  return gradeNumber >= 60 && gradeNumber <= 71
 
+}
+function isCoasting(gradeNumber) {
+  return gradeNumber >= 72 && gradeNumber <= 83
+}
+function isSucceeding(gradeNumber){
+  return gradeNumber >= 84 && gradeNumber <= 92
+}
+function isFailing(gradeNumber){
+  return gradeNumber < 60 ;
+} 
 
+function isAcing(gradeNumber) {
+  return gradeNumber > 92
+}
+
+function isStudent(myName) {
+  return "student" === myName
+}
+
+function isTeacher (myrole){
+  return 'teacher' === myrole
+}
+
+function isAdmin(myrole) {
+  return "admin" === myrole
+}
+function isElementary(myrole){
+return "elementary" === myrole
+} 
+function areDifferentPeople(nameOne , nameTwo){
+  return nameOne !== nameTwo
+}
+
+function isMiddleSchoolTeacher(role ,level) {
+ return isTeacher(role) &&  level >= 6  && level <= 8
+}
+function notAnElementarySchoolAdministrator(level,role){
+  return  "elementary" !== level || "admin" !== role
+} 
 
 /********************************************************************************************
  * CODE BELOW IS FOR EXPORTING THE VARIABLES AND FUNCTIONS YOU WROTE ABOVE TO MAIN.TEST.JS. *
